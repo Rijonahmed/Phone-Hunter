9c82a84c35e20f5c504fd6e8e696d1c76a77fe3a
Rijonahmed <rijon036@gmail.com>
2022-03-01 00:39:23 +0600

    secont comit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('everything is okk')
+const searchPhone = () => {
+  const searchFiled = document.getElementById('search-filed');
+  const searchText = searchFiled.value;
+  // console.log(searchText);
+
+  searchFiled.value = "";
+
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  // console.log(url);
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data.data))
+
+}
+
+const displaySearchResult = data => {
+  const searchResult = document.getElementById('search-result')
+}
