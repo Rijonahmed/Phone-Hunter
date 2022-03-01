5881efc3ec3229e6453d18b5141514e2a618ab49
Rijonahmed <rijon036@gmail.com>
2022-03-01 13:02:14 +0600

    phone details commit

--- js/app.js
+++ js/app.js
@@ -16,20 +16,21 @@ const searchPhone = () => {
 const displaySearchResult = data => {
   const searchResult = document.getElementById('search-result');
 
-  data.forEach(item => {
-    console.log(item)
+  data.forEach(phone => {
+    console.log(phone)
 
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
-   <div onclick ='loadMealDetail()'class="card">
+   <div class="card">
       <div class="container mt-3">
-       <img src="${item.image}" class="card-img-top " alt="...">
+       <img src="${phone.image}" class="card-img-top " alt="...">
       </div>
        <div class="card-body">
-         <h5 class="card-title">${item.phone_name}</h5>
-         <p class="card-text">${item.brand}</p>
-         <a href="#" class="btn btn-primary">Go somewhere</a>
+         <h5 class="card-title">${phone.phone_name}</h5>
+         <p class="card-text">${phone.brand}</p>
+         <button onclick ="loadPhoneDetail('${phone.slug}')" class="btn btn-primary">More Detiles</button>
+
        </div>
      </div>
    `;
@@ -37,4 +38,40 @@ const displaySearchResult = data => {
 
 
   })
+}
+
+
+const loadPhoneDetail = phoneId => {
+
+  const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayPhoneDetail(data.data))
+
+  console.log(phoneId)
+
+
+}
+
+const displayPhoneDetail = phoneDetail => {
+  console.log(phoneDetail);
+  const phoneDetails = document.getElementById('phone-details');
+  const div = document.createElement('div');
+  div.classList.add('row');
+  div.innerHTML = `
+  <div class="col-md-4 ">
+  <div class="container my-3">
+  <img src="${phoneDetail.image}" class="img-fluid rounded-start" alt="...">
+  </div>
+</div>
+<div class="col-md-8">
+  <div class="card-body">
+    <h5 class="card-title"><small>Brand Name : </small>${phoneDetail.brand}</h5>
+    <h5 class="card-title"><small>Model Name : </small>${phoneDetail.name}</h5>
+    <p class="card-text">Release Date : <small class="text-muted">${phoneDetail.releaseDate}</small></p>
+  </div>
+</div>
+  `;
+  phoneDetails.appendChild(div);
 }
