4c7fbea990979024ff481b6b5f63ba883b2a692a
Rijonahmed <rijon036@gmail.com>
2022-03-01 16:04:20 +0600

    other data commit

--- js/app.js
+++ js/app.js
@@ -70,7 +70,23 @@ const displayPhoneDetail = phoneDetail => {
     <h5 class="card-title"><small>Brand Name : </small>${phoneDetail.brand}</h5>
     <h5 class="card-title"><small>Model Name : </small>${phoneDetail.name}</h5>
     <p class="card-text">Release Date : <small class="text-muted">${phoneDetail.releaseDate}</small></p>
-  </div>
+    
+    <h4 class="card-title">More Features</h4>
+    <h6>&rtrif; <small>ChipSet</small> : ${phoneDetail.mainFeatures.chipSet}</h6>
+    <h6>&rtrif; <small>Display Size</small> : ${phoneDetail.mainFeatures.displaySize}</h6>
+    <h6>&rtrif; <small>Memory</small> : ${phoneDetail.mainFeatures.memory}</h6>
+    <h6>&rtrif; <small>Storage</small> : ${phoneDetail.mainFeatures.storage}</h6>
+    <h6>&rtrif; <small>Sensors</small> : ${phoneDetail.mainFeatures.sensors}</h6>
+
+    <h4>Others Features</h4>
+    <h6>&rtrif; <small>Bluetooth</small> : ${phoneDetail.others.Bluetooth}</h6>
+    <h6>&rtrif; <small>GPS</small> : ${phoneDetail.others.GPS}</h6>
+    <h6>&rtrif; <small>NFC</small> : ${phoneDetail.others.NFC}</h6>
+    <h6>&rtrif; <small>Radio</small> : ${phoneDetail.others.Radio}</h6>
+    <h6>&rtrif; <small>USB</small> : ${phoneDetail.others.USB}</h6>
+    <h6>&rtrif; <small>WLAN</small> : ${phoneDetail.others.WLAN}</h6>
+
+  </div> 
 </div>
   `;
   phoneDetails.appendChild(div);
