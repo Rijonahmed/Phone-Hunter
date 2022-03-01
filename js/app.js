cca7141fbaf8b199c6179555a45b3b45b218c157
Rijonahmed <rijon036@gmail.com>
2022-03-01 21:14:05 +0600

    final and last commit

--- js/app.js
+++ js/app.js
@@ -8,18 +8,13 @@ const searchPhone = () => {
   const searchText = searchFiled.value;
   // console.log(searchText);
   toggleSpinner('block');
-
-
   searchFiled.value = "";
-
-
 
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   // console.log(url);
   fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResult(data.data.slice(0, 20)))
-  // console.log(displaySearchResult(data.data));
 
 }
 
@@ -28,12 +23,14 @@ const displaySearchResult = data => {
 
 
   searchResult.textContent = '';
+  // error hending
   if (status == false) {
     document.getElementById('search-error').style.display = 'block';
 
   }
 
 
+  // for each data display
   data.forEach(phone => {
     console.log(phone)
     document.getElementById('search-error').style.display = 'none';
@@ -69,7 +66,7 @@ const loadPhoneDetail = phoneId => {
     .then(res => res.json())
     .then(data => displayPhoneDetail(data.data))
 
-  console.log(phoneId)
+  // console.log(phoneId)
 
 
 }
@@ -92,15 +89,20 @@ const displayPhoneDetail = phoneDetail => {
     <h1 class="card-title">${phoneDetail.name}</h1>
     <h5 class="card-title"><small>Brand Name : </small>${phoneDetail.brand}</h5>
     <p class="card-text">Release Date : <small class="text-muted">${phoneDetail.releaseDate ? phoneDetail.releaseDate : 'Comming soon'}</small></p>
+
     
     <h4 class="card-title">More Features</h4>
+
     <h6>&rtrif; <small>ChipSet</small> : ${phoneDetail.mainFeatures.chipSet}</h6>
     <h6>&rtrif; <small>Display Size</small> : ${phoneDetail.mainFeatures.displaySize}</h6>
     <h6>&rtrif; <small>Memory</small> : ${phoneDetail.mainFeatures.memory}</h6>
     <h6>&rtrif; <small>Storage</small> : ${phoneDetail.mainFeatures.storage}</h6>
     <h6>&rtrif; <small>Sensors</small> : ${phoneDetail.mainFeatures.sensors}</h6>
 
+
+
     <h4>Others Features</h4>
+
     <h6>&rtrif; <small>Bluetooth</small> : ${phoneDetail.others.Bluetooth}</h6>
     <h6>&rtrif; <small>GPS</small> : ${phoneDetail.others.GPS}</h6>
     <h6>&rtrif; <small>NFC</small> : ${phoneDetail.others.NFC}</h6>
